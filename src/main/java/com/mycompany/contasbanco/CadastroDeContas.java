68adeb325b9ce420d0747cac413e41fd57a41c02
erlison61 <99809349+erlison61@users.noreply.github.com>
2022-12-16 11:30:27 -0300

    Update CadastroDeContas.java

--- src/main/java/com/mycompany/contasbanco/CadastroDeContas.java
+++ src/main/java/com/mycompany/contasbanco/CadastroDeContas.java
@@ -7,28 +7,25 @@ public class CadastroDeContas{
     
     
     public CadastroDeContas(){
-<<<<<<< HEAD
+
         this.contas=new ArrayList<>();
-=======
-        contas = new ArrayList<>();
+     
     }
     
     public void adicionarConta(){
         contas.add(conta);  
->>>>>>> 674c2082b7b7c04eb1d1628363220f0a6c1dab63
+
     }
     
     public void addContas(Conta conta){
         contas.add(conta);
     }
     
-<<<<<<< HEAD
+
     public Conta searchConta(String nome){
         for(Conta conta:contas){
            if(conta.getNomeDoCliente().equals(nome)) return conta;
         }
         return null;
     }
-=======
->>>>>>> 674c2082b7b7c04eb1d1628363220f0a6c1dab63
 }
